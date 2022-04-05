12e069eade91e686e3a5727e4d5da948f652d918
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2022-04-05 20:41:15 +0300

    muokkausta

--- budjet-app/src/database_connection.py
+++ budjet-app/src/database_connection.py
@@ -2,9 +2,12 @@ import os
 import sqlite3
 
 dirname = os.path.dirname(__file__)
+print("MYDIR = ", os.path.realpath(dirname))
 
 connection = sqlite3.connect(os.path.join(dirname, "..", "data", "database.sqlite"))
 connection.row_factory = sqlite3.Row
+print("THE REAL SQLPATH is", os.path.realpath(connection))
+
 
 
 def get_database_connection():
