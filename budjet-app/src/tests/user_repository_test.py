8b87da01de257965447a1cf239f1138365242a9d
Sanni Tuomisto <101722915+sannituomisto@users.noreply.github.com>
2022-04-05 21:54:22 +0300

    Update user_repository_test.py

--- budjet-app/src/tests/user_repository_test.py
+++ budjet-app/src/tests/user_repository_test.py
@@ -8,6 +8,3 @@ class TestUserRepository(unittest.TestCase):
         self.user_1 = User('user1', 'user123')
         self.user_2 = User('user2', 'user456')
 
-    def test_create(self):
-        user_new=self.testrepo.create_user(self.user_1)
-        self.assertEqual(user_new, "User created")
