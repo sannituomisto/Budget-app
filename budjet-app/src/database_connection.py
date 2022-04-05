221f8feb628e56eee488afb7518938df5f954843
Sanni Tuomisto <101722915+sannituomisto@users.noreply.github.com>
2022-04-05 23:16:17 +0300

    Update database_connection.py

--- budjet-app/src/database_connection.py
+++ budjet-app/src/database_connection.py
@@ -2,12 +2,8 @@ import os
 import sqlite3
 from config import DB_FILE_PATH
 
-
 connection = sqlite3.connect(DB_FILE_PATH)
 connection.row_factory = sqlite3.Row
-
-
-
 
 def get_database_connection():
     return connection
