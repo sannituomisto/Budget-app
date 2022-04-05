d58f387e4f508b8e5d2071ac5e632d63c4206d96
Sanni Tuomisto <101722915+sannituomisto@users.noreply.github.com>
2022-04-05 23:25:12 +0300

    Update budjet_services.py

--- budjet-app/src/services/budjet_services.py
+++ budjet-app/src/services/budjet_services.py
@@ -1,5 +1,4 @@
 from entities.user import User
-
 from repositories.user_repository import user_repository
 
 class UsernameExistsError(Exception):
@@ -7,19 +6,13 @@ class UsernameExistsError(Exception):
 
 class BudjetServices:
     def __init__(self, user_repository):
-        #self._user = None
         self._user_repository = user_repository
 
     def create_user(self, username, password):
-
-        existing_user = self._user_repository.find_by_username(username)
-
-        if existing_user:
-            raise UsernameExistsError(f'Username {username} already exists')
-
+        user_existing = self._user_repository.find_by_username(username)
+        if user_existing:
+            raise UsernameExistsError(f'This username already exists')
         user = self._user_repository.create(User(username, password))
-
-
         return user
 
 
