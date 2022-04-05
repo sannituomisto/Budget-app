fc2859aa1b701b6533d88ae24a13ecf399d0f163
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2022-04-05 21:31:12 +0300

    Lisätään data

--- budjet-app/src/database_connection.py
+++ budjet-app/src/database_connection.py
@@ -2,10 +2,10 @@ import os
 import sqlite3
 from config import DATABASE_FILE_PATH
 
-
+print("THE REAL SQLPATH is", os.path.realpath(DATABASE_FILE_PATH))
 connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
-print("THE REAL SQLPATH is", os.path.realpath(connection))
+
 
 
 
