256dbf02c5453e6ef63f658d3219e8e98e202384
Sanni Tuomisto <101722915+sannituomisto@users.noreply.github.com>
2022-04-05 23:15:31 +0300

    Update initialize_database.py

--- budjet-app/src/initialize_database.py
+++ budjet-app/src/initialize_database.py
@@ -1,28 +1,20 @@
 from database_connection import get_database_connection
 
-
 def drop_tables(connection):
     cursor = connection.cursor()
-
     cursor.execute("DROP TABLE IF EXISTS Users;")
-
     connection.commit()
-
 
 def create_tables(connection):
     cursor = connection.cursor()
-
     cursor.execute("CREATE TABLE Users (username TEXT PRIMARY KEY,password TEXT);")
-
     connection.commit()
 
 
 def initialize_database():
     connection = get_database_connection()
-
     drop_tables(connection)
     create_tables(connection)
-
 
 if __name__ == "__main__":
     initialize_database()
