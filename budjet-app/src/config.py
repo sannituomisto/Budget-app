6ad36883eb5010a488c8bb1b29c3b48cb4ac9c35
Sanni Tuomisto <101722915+sannituomisto@users.noreply.github.com>
2022-04-05 23:17:06 +0300

    Update config.py

--- budjet-app/src/config.py
+++ budjet-app/src/config.py
@@ -1,8 +1,7 @@
-import os
 from dotenv import load_dotenv
+import os
 
 dirname = os.path.dirname(__file__)
-
 try:
     load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
 except FileNotFoundError:
